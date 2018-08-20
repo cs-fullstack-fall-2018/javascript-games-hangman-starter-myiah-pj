506dc4e3ddfe0e5cd1f94fc442d2f13b3d9099f9
Myiah Moore <myiahsade@gmail.com>
2018-08-20 14:51:36 -0500

    check in

--- js.js
+++ js.js
@@ -1,11 +1,51 @@
-window.onload = function () {
-
-    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];}
 var wordList = ["cat", "dog", "fish" ];
+window.onload;selectWord(); //loading a new random word everytime the page loads
+var guessedLetters ="";
+String.prototype.replaceAt=function(index, replacement)
+{
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
 
- function selectWord()
- {
- Math.random();
- }
+    function selectWord()
+    {
+        randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+        console.log(randomWord);
+        masked="";
+
+        for(i=0;i<randomWord.length;i++)
+        {
+            masked += "*";
+        }
+
+
+        console.log(masked);
+    }
+function guesses()
+{
+    var userInput = prompt("Guess a letter: ");
+    if (guessedLetters.indexOf(userInput) > -1)
+    {
+        alert("Already guessed that letter "+userInput);
+        return;
+    }
+
+    guessedLetters += userInput;
+
+    console.log("guessed letters = "+guessedLetters);
+
+    var letterPosition = randomWord.indexOf(userInput);
+    if (letterPosition> -1)
+{
+    console.log("found the letter at " + letterPosition);
+    masked = masked.replaceAt(letterPosition,userInput);
+    console.log(masked);
+
+
+    alert(userInput);
+
+}
+
+}
+while(true) {
+    guesses();
+}
